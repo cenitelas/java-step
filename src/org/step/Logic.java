f1cf680305afec93d3b7d46dc59bab67727c13ca
cenitelas <sanyaug777888>
2020-05-04 04:46:07 +0600

    Initial commit

---
+++ src/org/step/Logic.java
@@ -0,0 +1,7 @@
+package org.step;
+
+public class Logic {
+    public String method(){
+        return "I am string in Logic.";
+    }
+}
---
+++ src/org/step/Main.java
@@ -0,0 +1,9 @@
+package org.step;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Logic logic = new Logic();
+        System.out.println(logic.method());
+    }
+}
